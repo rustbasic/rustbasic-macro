16e8723e194946da33c9a1f5d0916393221ced2b
rustbasic <127506429+rustbasic@users.noreply.github.com>
2023-03-19 16:09:34 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -3,106 +3,164 @@
 //! `RustBasic` is a planned development that aims to make Rust easy to learn, teach, and use.
 
 // rustbasic macro - lib.rs
+// Thanks to wusyong & team ( https://github.com/wusyong/smol-potat ) for the reference.
+// Thanks to Fishrock123 & team ( https://github.com/async-rs/async-attributes ) for the reference.
 #![allow(unused_doc_comments)]
-#![allow(unused_imports)]
 #![allow(dead_code)]
 
-// #[macro_use]
-// mod utils;
+#![allow(unused_imports)]
 
-/// # How to use
-/// 
-/// ## 1.
-/// ```ignore
-/// stopwatch_start!();
-/// sleeps(1);
-/// stopwatch_stop!();
-/// ```
-/// ## 2.
-/// ```ignore
-/// let mut stopwatch = Stopwatch::new();
-/// stopwatch.start();
-/// sleeps(1);
-/// stopwatch.stop();
-/// ```
+use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::{quote, quote_spanned};
+use syn::parse::{Parse, ParseStream};
+use syn::spanned::Spanned;
+use syn::Expr;
 
-// rust basic macro - macros.rs
-use std::time::Instant;
-use std::time::Duration;
-use std::sync::Mutex;
-// use crate::utils::*;
+#[proc_macro_attribute]
+pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+    let opts = syn::parse_macro_input!(attr as Opts);
 
-lazy_static::lazy_static! {
-    static ref STOPWATCH: Mutex<Option<STOPWATCH>> = Mutex::new(None);
-}
+    let ret = &input.sig.output;
+    let inputs = &input.sig.inputs;
+    let name = &input.sig.ident;
+    let body = &input.block;
+    #[allow(unused_variables)]
+    let attrs = &input.attrs;
+    let vis = &input.vis;
 
-struct Stopwatch {
-    start_time: Instant,
-    stop_time: Instant,
-}
+    let crate_root = opts.crate_root;
 
-impl Stopwatch {
+    let threads = match opts.threads {
+        Some((num, span)) => {
+            let num = num.to_string();
+            Some(quote_spanned!(span=> #num))
+        }
+        #[cfg(feature = "auto")]
+        None => Some(quote! {
+            #crate_root::std::string::ToString::to_string(
+                &#crate_root::std::cmp::max(#crate_root::num_cpus::get(), 1)
+            )
+        }),
+        #[cfg(not(feature = "auto"))]
+        None => None,
+    };
 
-    pub fn new() -> Stopwatch {
-        println!("Stopwatch Start...");
-        Stopwatch {
-            start_time: Instant::now(),
-            stop_time: Instant::now(),
+    #[allow(unused_variables)]
+    let set_threads = threads.map(|threads| {
+        quote! {
+            #crate_root::std::env::set_var(
+                "RUSTBASIC-THREADS",
+                #threads,
+            );
+        }
+    });
+
+    let result : proc_macro2::TokenStream;
+
+    match input.sig.asyncness {
+        None | Some(_) if input.sig.ident != "main" => {    // => If it is not async or not main(),
+            result = quote! {
+                #vis fn #name(#inputs) #ret {
+                    println!("Hello, macro attribute.");
+                    #body
+                }
+            }
+        }
+        _ => {                                              //  => If it is async main().
+            result = quote! {
+                #vis fn main() #ret {
+                    println!("Hello, macro attribute.");
+                    futures::executor::block_on(async_main());
+                }
+                async fn async_main() #ret {
+                    #body
+                }
+            }
         }
     }
 
-    pub fn start(&mut self) {
-        self.start_time = Instant::now();
-        self.stop_time = self.start_time;
-    }
-
-    pub fn stop(&mut self) {
-        self.stop_time = Instant::now();
-        let elapsed_time = self.duration();
-        println!("Stopwatch Stop... Elapsed time: {:?}", elapsed_time);
-    }
-
-    pub fn duration(&self) -> Duration {
-        self.stop_time - self.start_time
-    }
+    result.into()
 }
 
-/*
-#[macro_export]
-macro_rules! stopwatch_start {
-    () => {
-        let stopwatch = Stopwatch::new();
-        *STOPWATCH.lock().unwrap() = Some(stopwatch);
-    };
+struct Opts {
+    crate_root: syn::Path,
+    threads: Option<(u32, Span)>,
 }
 
-#[macro_export]
-macro_rules! stopwatch_stop {
-    () => {
-        let mut stopwatch_option = STOPWATCH.lock().unwrap();
-        if let Some(ref mut stopwatch) = *stopwatch_option {
-            stopwatch.stop();
+impl Parse for Opts {
+    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+        let mut crate_root = None;
+        let mut threads = None;
+
+        loop {
+            if input.is_empty() {
+                break;
+            }
+
+            let name_value: syn::MetaNameValue = input.parse()?;
+            let ident = match name_value.path.get_ident() {
+                Some(ident) => ident,
+                None => {
+                    return Err(syn::Error::new_spanned(
+                        name_value.path,
+                        "Must be a single ident",
+                    ))
+                }
+            };
+            match &*ident.to_string().to_lowercase() {
+                "threads" => match &name_value.lit {
+                    syn::Lit::Int(expr) => {
+                        if threads.is_some() {
+                            return Err(syn::Error::new_spanned(
+                                name_value,
+                                "multiple threads argments",
+                            ));
+                        }
+
+                        let num = expr.base10_parse::<std::num::NonZeroU32>()?;
+                        threads = Some((num.get(), expr.span()));
+                    }
+                    _ => {
+                        return Err(syn::Error::new_spanned(
+                            name_value,
+                            "threads argument must be an integer",
+                        ))
+                    }
+                },
+                "crate" => match &name_value.lit {
+                    syn::Lit::Str(path) => {
+                        if crate_root.is_some() {
+                            return Err(syn::Error::new_spanned(
+                                name_value,
+                                "multiple crate arguments",
+                            ));
+                        }
+
+                        crate_root = Some(path.parse()?);
+                    }
+                    _ => {
+                        return Err(syn::Error::new_spanned(
+                            name_value,
+                            "crate argument must be a string",
+                        ))
+                    }
+                },
+                name => {
+                    return Err(syn::Error::new_spanned(
+                        name,
+                        "unknown attribute {}, expected `threads` or `crate`",
+                    ));
+                }
+            }
+
+            input.parse::<Option<syn::Token![,]>>()?;
         }
-        *stopwatch_option = None;
-    };
-}
-*/
 
-#[test]
-fn test_stopwatch() {
-    use std::thread;
-
-    let sleeps = |second| {
-        let timeout = Duration::from_secs(second);
-        thread::park_timeout(timeout);
-    };
-
-//    stopwatch_start!();
-//    sleeps(1);
-//    stopwatch_stop!();
-
-    let mut stopwatch = Stopwatch::new();
-    stopwatch.start();
-    sleeps(1);
-    stopwatch.stop();
+        Ok(Self {
+            crate_root: crate_root.unwrap_or_else(|| syn::parse2(quote!(::rustbasic)).unwrap()),
+            threads,
+        })
+    }
 }
